4ba5d07cd848df6c6ab231d50d6b39684e4185b5
David AQ <153019747@qq.com>
2012-01-17 19:37:44 +0800

    start writing point corr. using scene coherence

---
+++ msrc/pointSceneCoherence/pointSceneCoherence_fetchPoints.m
@@ -0,0 +1,11 @@
+function pointList = pointSceneCoherence_fetchPoints( inFile )
+%POINTSCENECOHERENCE_FETCHPOINTS get the list of points in a urw file
+%    Input:    the input file inFile
+%    Output:    a list of points
+%    Author:    Davidaq
+%    Date:    2012.01.17
+%    Reference:   
+
+
+end
+
---
+++ msrc/pointSceneCoherence/pointSceneCoherence_transfunc.m
@@ -0,0 +1,26 @@
+function [ a,b,c,d,e,f,g,h,i,j,k,l ] = pointSceneCoherence_transfunc( pointList1 , pointList2 )
+%POINTSCENECOHERENCE_TRANSFUNC get the paramaters of a function using 4
+%       pairs of points
+%    Input:    2 point lists
+%    Output:    a transformation function maping from list1 to list2
+%    Author:    Davidaq
+%    Date:    2012.01.17
+%    Reference:   
+%    X=ax+by+cz+d
+%    Y=ex+fy+gz+h
+%    Z=ix+jy+kz+l
+
+lt=[pointList1(1).x,pointList1(1).y,pointList1(1).z,1;
+    pointList1(2).x,pointList1(2).y,pointList1(2).z,1;
+    pointList1(3).x,pointList1(3).y,pointList1(3).z,1;
+    pointList1(4).x,pointList1(4).y,pointList1(4).z,1;
+    ];
+
+rt=[pointList2(1).x;pointList2(2).x;pointList2(3).x;pointList2(4).x];
+[a,b,c,d]=lt\rt;
+rt=[pointList2(1).y;pointList2(2).y;pointList2(3).y;pointList2(4).y];
+[e,f,g,h]=lt\rt;
+rt=[pointList2(1).z;pointList2(2).z;pointList2(3).z;pointList2(4).z];
+[i,j,k,l]=lt\rt;
+
+end
