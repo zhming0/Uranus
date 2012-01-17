5704f022676b4df74da3393e15e112cb53dcf8f8
Zheng Jingjie <i@tsenmu.com>
2012-01-17 12:53:48 +0800

    public_defaultNumber.m has been deleted due to the new rule.
    Signed-off-by: Zheng Jingjie <i@tsenmu.com>

--- msrc/public/public_defaultNumber.m
+++
@@ -1,9 +0,0 @@
-function res = public_defaultNumber()
-%PUBLIC_DEFAULTNUMBER    Return the default number.
-%    Input:    
-%    Output:    The default number.
-%    Author:    Tsenmu
-%    Date:    2012.01.12
-%    Reference:    
-    res = -Inf;
-end
