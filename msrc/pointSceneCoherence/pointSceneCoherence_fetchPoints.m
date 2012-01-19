b25f54cc214b8908875f975bf2f0afc85f64fb84
David AQ <153019747@qq.com>
2012-01-19 14:48:33 +0800

    Almost finishing points corr using scene coherence

--- msrc/pointSceneCoherence/pointSceneCoherence_fetchPoints.m
+++ msrc/pointSceneCoherence/pointSceneCoherence_fetchPoints.m
@@ -3,9 +3,19 @@ function pointList = pointSceneCoherence_fetchPoints( inFile )
 %    Input:    the input file inFile
 %    Output:    a list of points
 %    Author:    Davidaq
-%    Date:    2012.01.17
+%    Date:    2012.01.19
 %    Reference:   
 
-
+[dataset,ps]=public_urw2dataset(inFile);
+[c,r,t,h]=size(dataset);
+pointList=[];
+for z=1:h
+    for x=1:c
+        for y=1:r
+            if dataset(x,y,1,z)>100
+                pointList=cat(pointList,struct('x',x,'y',y,'z',z));
+            end
+        end
+    end
 end
 
