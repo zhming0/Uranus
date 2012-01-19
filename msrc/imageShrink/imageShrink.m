068cffa9053d48d12027b25f698154006e4f9065
Ming.G <mjzshd@gmail.com>
2012-01-19 23:59:17 +0800

    Fixed severe bugs for image shrink

--- msrc/imageShrink/imageShrink.m
+++ msrc/imageShrink/imageShrink.m
@@ -7,9 +7,7 @@ function [res] = imageShrink(dataset,post_x,post_y,post_z)
 %    Date:    2012.01.18
 %    Reference:    
     [l1 l2 l3 l4] = size(dataset);
-    count_x = 1;
-    count_y = 1;
-    count_z = 1;
+    
     gap_x = uint16(l1/post_x);
     gap_y = uint16(l2/post_y);
     gap_z = uint16(l4/post_z);
@@ -23,31 +21,32 @@ function [res] = imageShrink(dataset,post_x,post_y,post_z)
         gap_z = gap_z + 1;
     end
     
-    b = zeros(post_x,post_y,1,post_z);
+    res = uint8(zeros(post_x, post_y, 1, post_z));
     
+    count_x = 1;
     for i = 1:post_x
-        io_progress(double(i)/double(post_x));
+        count_y = 1;
         for j = 1:post_y
+            count_z = 1;
             for k = 1:post_z
-                tmp_x = mod(count_x, l1);
-                tmp_y = mod(count_y, l2);
-                tmp_z = mod(count_z, l4);
-                if tmp_x == 0
-                    tmp_x = l1;
+                if count_x > l1
+                    count_x = l1;
                 end
-                if tmp_y == 0
-                    tmp_y = l2;
+                if count_y > l2
+                    count_y = l2;
                 end
-                if tmp_z == 0
-                    tmp_z = l4;
+                if count_z > l4
+                    count_z = l4;
                 end
-                b(i, j, 1, k) = dataset(tmp_x, tmp_y, 1, tmp_z);
-                count_x = count_x+gap_x;
+                res(i, j, 1, k) = dataset(count_x, count_y, 1, count_z);
+                %[res(i, j, 1, k) count_x count_y count_z]
+                count_z = count_z+gap_z;
             end
-            count_y = count_y+gap_y;
+            count_y = count_y + gap_y;
         end
-        count_z = count_z+gap_z;
+        count_x = count_x+gap_x;
+        io_progress(double(i)/double(post_x));
     end
-    res = b;
+    
 end
 
