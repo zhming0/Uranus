6d5e434599d89af7d6c9f4bfe33808a3251241b1
David AQ <153019747@qq.com>
2012-01-24 14:58:41 +0800

    Some updates.

--- msrc/pointSceneCoherence/pointSceneCoherence.m
+++ msrc/pointSceneCoherence/pointSceneCoherence.m
@@ -1,12 +1,11 @@
-function func = pointSceneCoherence( plist1 , plist2 , tol , dataset , ps )
+function func = pointSceneCoherence( plist1 , plist2 , dataset , ps )
 %POINTSCENECOHERENCE main function of point correspondece using scene
 %                   coherence
 %    Input:    2 list of points and a point distance error tolerance
 %    Output:    
 %    Author:    Davidaq
 %    Date:    2012.01.19
-%    Reference:       
-tol=tol*tol;
+%    Reference: 
 
 len1=length(plist1);
 len2=length(plist2);
@@ -15,34 +14,35 @@ func='';
 % random point selection
 clen=min([len1 len2]);
 max=10000;
-if(clen<max)
-    perm1=randperm(clen);
-    perm2=randperm(clen);
+perm1=[];
+perm2=[];
+perm1=randperm(clen);
+perm2=randperm(clen);
+
+if(clen>25)
+    lll=25;
 else
-    d=clen-max;
-    perm1=uint32(rand*d)+randperm(max);
-    perm2=uint32(rand*d)+randperm(max);
-    clen=max;
+    lll=clen;
 end
+comb=combntns(1:lll,4);
+clen=length(comb);
+
 resCount=0;
 resList1=[];
 resList2=[];
     % select points
-    for sel=1:clen-3
-        pl1=plist1(perm1(sel:sel+3));
-        pl2=plist2(perm2(sel:sel+3));
+    for sel=1:clen
+        io_progress(0.1+0.75*double(sel)/double(clen));
+        cb=comb(sel,:);
+        pl1=plist1(perm1(cb));
+        pl2=plist2(perm2(cb));
         clist1=[];
         clist2=[];
         count=0;
         % get a transformation function
         [a,b,c,d,e,f,g,h,i,j,k,l,bad]=pointSceneCoherence_transfunc(pl1,pl2);
         if(bad)
-            if(bitand(bad,1))
-                break;
-            end
-            if(bitand(bad,2))
-                continue;
-            end
+            continue;
         end
         % check if this one works
         for m=1:len1
@@ -57,7 +57,9 @@ resList2=[];
                     count=count+1;
                     clist1=cat(1,clist1,plist1(m));
                     clist2=cat(1,clist2,struct('x',double(X)*ps(1),'y',double(Y)*ps(2),'z',double(Z)*ps(3)));
+                    dataset(X,Y,1,Z)=dataset(X,Y,1,Z)-150;
                 else
+                    br=false;
                     for x=-1:1
                         for y=-1:1
                             for z=-1:1
@@ -66,11 +68,21 @@ resList2=[];
                                         count=count+1;
                                         clist1=cat(1,clist1,plist1(m));
                                         clist2=cat(1,clist2,struct('x',double(X)*ps(1),'y',double(Y)*ps(2),'z',double(Z)*ps(3)));
-                                        break;
+                                        dataset(X+x,Y+y,1,Z+z)=dataset(X+x,Y+y,1,Z+z)-150;
+                                        br=true;
                                     end
                                 catch
                                 end
+                                if br
+                                    break;
+                                end
                             end
+                            if br
+                                break;
+                            end
+                        end
+                        if br
+                            break;
                         end
                     end
                 end
@@ -92,17 +104,19 @@ if(count==0)
     io_alert('Faild to correspond features');
     return;
 end
-comb=combntns(1:count,4);
-len=length(comb);
-perm=randperm(len);
-if(len>30)
-    len=30;
+if(count>20)
+    len=20;
+else
+    len=count;
 end
+perm=randperm(count);
+comb=nchoosek(perm(1:len),4);
+len=length(comb);
 a=0;b=0;c=0;d=0;e=0;f=0;g=0;h=0;i=0;j=0;k=0;l=0;
 n=0;
 io_progress(0.9);
 for m=1:len
-    [a2,b2,c2,d2,e2,f2,g2,h2,i2,j2,k2,l2,bad]=pointSceneCoherence_transfunc(clist1(comb(perm(m),:)),clist2(comb(perm(m),:)));
+    [a2,b2,c2,d2,e2,f2,g2,h2,i2,j2,k2,l2,bad]=pointSceneCoherence_transfunc(clist1(comb(m,:)),clist2(comb(m,:)));
     if(bad)
         continue;
     end
--- msrc/pointSceneCoherence/pointSceneCoherence_entry.m
+++ msrc/pointSceneCoherence/pointSceneCoherence_entry.m
@@ -7,17 +7,15 @@ function pointSceneCoherence_entry( )
 %    Reference:   
 
 %inFile=char(input(''));
-inFile='C:\Users\acer\Desktop\ct_left_centre.urw';
+inFile='C:\Users\acer\Desktop\ct_right_centre.urw';
 %outFile=char(input(''));
 outFile='E:\ElseIf\try.urw';
 %sFile=io_getfile('*.urw','Please choose the sample image');
 sFile='C:\Users\acer\Desktop\mr_centre.urw';
-%tol=io_prompt(2,'Tolerance exmamining points');
-tol=2;
 pl1=pointSceneCoherence_fetchPoints(inFile);
 [pl2,dataset,ps]=pointSceneCoherence_fetchPoints(sFile);
 io_progress(0.1);
-func=pointSceneCoherence(pl1,pl2,tol,dataset,ps)
+func=pointSceneCoherence(pl1,pl2,dataset,ps)
 fp=fopen(outFile,'w');
 fwrite(fp,uint8(zeros(1,5)),'uint8');
 fwrite(fp,func);
