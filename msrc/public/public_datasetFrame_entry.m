517d61219bf8e2a77395bdb3bd7a8b9077e52887
Zheng Jingjie <i@tsenmu.com>
2012-01-25 13:05:07 +0800

    Implement an entry to datasetFrame
    Signed-off-by: Zheng Jingjie <i@tsenmu.com>

---
+++ msrc/public/public_datasetFrame_entry.m
@@ -0,0 +1,10 @@
+function [] = public_datasetFrame_entry()
+    inFileName = input('');
+    outFileName = input('');
+    [ds_in, ps_in] = public_urw2dataset(inFileName);
+    io_progress(0.2);
+    [ds_in, ps_in] = datasetFrame(ds_in, ps_in);
+    io_progress(0.5);
+    public_dataset2urw(outFileName, ds_in, ps_in);
+    io_progress(1.0);
+end
