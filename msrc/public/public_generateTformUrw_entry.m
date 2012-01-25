3b957b35d9e7dded5dfa6bd27c95413ec70b7fdf
Zheng Jingjie <i@tsenmu.com>
2012-01-25 15:07:20 +0800

    Update.
    Signed-off-by: Zheng Jingjie <i@tsenmu.com>

--- msrc/public/public_generateTformUrw_entry.m
+++ msrc/public/public_generateTformUrw_entry.m
@@ -2,6 +2,6 @@ function public_generateTformUrw_entry()
     inFileName = input('');
     outFileName = input('');
     urwpath = io_prompt('.\Tform.urw', 'Please enter the path of urw to be saved:');
-    tform = io_prompt('[1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1]', 'Please enter the transform matrix:');
-    generateTformUrw(urwpath, tform);
+    tform = double(io_prompt('[0.4972   -0.2722   -0.5375  138.1875;0.2889   -0.6889   -0.1000  174.5000;  0.9806    1.0944    0.9875 -272.4375]', 'Please enter the transform matrix:'));
+    public_generateTformUrw(urwpath, tform);
 end
