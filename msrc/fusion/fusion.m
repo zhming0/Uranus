40d67608ec8cbfc3fed81a47ad6fcef98efc5c17
Zheng Jingjie <i@tsenmu.com>
2012-01-25 13:03:51 +0800

    Fusion updated.
    Signed-off-by: Zheng Jingjie <i@tsenmu.com>

--- msrc/fusion/fusion.m
+++ msrc/fusion/fusion.m
@@ -1,4 +1,12 @@
-function [ds_F, ps_F] = fusion(ds_A, ps_A, ds_B, ps_B)
-
-
+function [ds_F, ps_F] = fusion(ds_CT, ps_CT, ds_MR, ps_MR, CT_Thresh)
+    ps_F = [];
+    warn = false;
+    if(size(ds_CT) ~= size(ds_MR)) 
+       io_alert('The size of two images is not the same.'); 
+       warn = true;
+    end
+    if(~warn) 
+        [ds_CT, ~] = fusion_boneSelection(ds_CT,ps_CT, CT_Thresh);
+        ds_F = uint8(double(ds_CT) * 0.8 + double(ds_MR));
+    end
 end
