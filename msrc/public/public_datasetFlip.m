9f4421bb71c1e248e91d42f7feeeebbb1bcedc7e
Zheng Jingjie <i@tsenmu.com>
2012-01-26 10:35:29 +0800

    An update enhancing the efficiency of public/public_datasetFlip.m.
    Signed-off-by: Zheng Jingjie <i@tsenmu.com>

--- msrc/public/public_datasetFlip.m
+++ msrc/public/public_datasetFlip.m
@@ -12,19 +12,31 @@ function [ds_r, ps_r] = public_datasetFlip(ds_in, ps_in, orientation)
     ds_in = squeeze(ds_in);
     [r c h] = size(ds_in);
     ds_r = uint8(zeros([r c h]));
+    switch(lower(orientation))
+        case 'updown'
         for ri = 1 : r
             for ci = 1 : c
-                for z = 1 : h
-                    switch(lower(orientation))
-                        case 'updown'
+                for z = 1 : h     
                             ds_r(ri, ci, h - z + 1) = ds_in(ri, ci, z);
-                        case 'leftright'
-                            ds_r(ri, c - ci + 1, z) = ds_in(ri, ci, z);
-                        case 'frontback'
-                            ds_r(r - ri + 1, ci, z) = ds_in(ri, ci, z);
-                    end
                 end
             end
         end
+        case 'leftright'
+        for ri = 1 : r
+            for ci = 1 : c
+                for z = 1 : h
+                            ds_r(ri, c - ci + 1, z) = ds_in(ri, ci, z);
+                end
+            end
+        end
+        case 'frontback'
+        for ri = 1 : r
+            for ci = 1 : c
+                for z = 1 : h
+                            ds_r(r - ri + 1, ci, z) = ds_in(ri, ci, z);
+                end
+            end
+        end
+    end
     ds_r = public_expand(ds_r);
 end
